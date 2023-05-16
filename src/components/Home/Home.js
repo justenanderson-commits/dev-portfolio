cc594e633aa0c4c3acd154d256f5c9a515c0d9c5
Justen Anderson <justenanderson@gmail.com>
2023-05-16 13:00:07 -0600

    Add initial home content.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
-  return ( <>
-    Home Content
-  </>  );
+  return (
+    <div className="text-slate-100 text-2xl w-full h-screen h-9/10 bg-slate-900">
+      <h1 className="text-7xl text-center">Justen Anderson</h1>
+      <h2 className="text 4xl text-center text-sky-300">Software Engineer</h2>
+      <p className="text-slate-300 text-center">Passionate learner and hard worker with a strong record of success in the military, education, and private industry. I value honesty, deep connections, and achievement. I love collaborating, mentoring, and learning from others. Pair-programming is preferred. Complex technical challenges excite me, and I work to purposefully improve products, processes, and the lives of other people.</p>
+    </div>
+  )
 }
- 
-export default Home;
+
+export default Home
