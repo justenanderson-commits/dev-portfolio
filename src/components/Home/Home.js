99032654ca124e7e90477474af964800d7c64df9
Justen Anderson <justenanderson@gmail.com>
2023-05-16 13:06:03 -0600

    Style initial home content.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,16 @@
 const Home = () => {
   return (
     <div className="text-slate-100 text-2xl w-full h-screen h-9/10 bg-slate-900">
-      <h1 className="text-7xl text-center">Justen Anderson</h1>
-      <h2 className="text 4xl text-center text-sky-300">Software Engineer</h2>
-      <p className="text-slate-300 text-center">Passionate learner and hard worker with a strong record of success in the military, education, and private industry. I value honesty, deep connections, and achievement. I love collaborating, mentoring, and learning from others. Pair-programming is preferred. Complex technical challenges excite me, and I work to purposefully improve products, processes, and the lives of other people.</p>
+      <h1 className="text-7xl text-center pt-20 pb-5">Justen Anderson</h1>
+      <h2 className="text-4xl text-center text-sky-300">Software Engineer</h2>
+      <p className="text-slate-300 p-20 text-left">
+        Passionate learner and hard worker with a strong record of success in
+        the military, education, and private industry. I value honesty, deep
+        connections, and achievement. I love collaborating, mentoring, and
+        learning from others. Pair-programming is preferred. Complex technical
+        challenges excite me, and I work to purposefully improve products,
+        processes, and the lives of other people.
+      </p>
     </div>
   )
 }
