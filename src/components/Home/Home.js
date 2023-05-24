040d4bb7ca624200b8893dea10c77a3818b41a56
Justen Anderson <justenanderson@gmail.com>
2023-05-24 12:31:04 -0600

    Fix margins on Home paragraph.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="text-slate-100 text-2xl w-full h-screen h-9/10 bg-slate-900 pt-20 flex flex-col items-center">
       <h1 className="text-7xl text-center pb-5">Justen Anderson</h1>
       <h2 className="text-4xl text-center text-sky-300">Software Engineer</h2>
-      <p className="text-slate-300 pt-5 px-20 text-left">
+      <p className="text-slate-300 pt-5 px-32 text-left">
         Passionate learner and hard worker with a strong record of success in
         the military, education, and private industry. I value honesty, deep
         connections, and achievement. I love collaborating, mentoring, and
