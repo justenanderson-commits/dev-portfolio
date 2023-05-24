efc0e0ec3d05affeeeb1091c6197ca2aff7b7adf
Justen Anderson <justenanderson@gmail.com>
2023-05-24 17:07:44 -0600

    Style project cards.

--- src/components/ProjCard/ProjCard.js
+++ src/components/ProjCard/ProjCard.js
@@ -1,6 +1,6 @@
 const ProjCard = () => {
   return (
-    <div className="w-3/4 border-white border-2 m-10 items-center">
+    <div className="w-3/4 border-white border-2 rounded-xl mx-auto my-10">
       <h2 className="text-1xl text-center text-sky-300 pt-2">Menuify</h2>
       <p className="text-slate-300 py-5 px-5 text-left">
         Menu application for restaurant owners to create beautiful, intuitive
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,7 @@ const Projects = () => {
   return (
     <div className="text-slate-100 text-2xl w-full h-screen h-9/10 bg-slate-900 pt-20 flex flex-col items-center">
       <h1 className="text-7xl text-center pb-5">Projects</h1>
-      <div className="grid grid-cols-2 gap-4 items-center">
+      <div className="grid grid-cols-2 gap-0">
       <ProjCard />
       <ProjCard />
       <ProjCard />
