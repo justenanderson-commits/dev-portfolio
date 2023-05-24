bf3ce01622f502aec1d49b20d47c01847e5b0131
Justen Anderson <justenanderson@gmail.com>
2023-05-24 09:12:12 -0600

    Add summary paragraph.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,13 +2,7 @@ const About = () => {
   return (
     <div className="text-slate-900 text-2xl w-full h-screen h-9/10 bg-white pt-10">
       <h1 className="text-7xl text-center pb-5">About</h1>
-      <div className="mx-20 my-10">I am a kind, hard-working person with a strong record of successful teamwork in the military, education, and private industry. Collaborating, mentoring, and learning from others feeds my soul and fuels my success. I value honesty, deep connections and achievement.
-<br></br><br></br>
-A voracious learner, I continually reach for new skills and experiences that will help me add value to my organization and broader community. If I'm without the skills to complete a project, I eagerly deep-dive into research and practice until I have the skillset needed to succeed.<br></br><br></br>
-
-Complex and technical challenges excite me. I am fueled by the sense of achievement from persevering and overcoming struggles. Excellence, integrity, and service are my personal core values. I work with purpose to improve products, processes, and the lives of people around me.
-<br></br><br></br>
-Oh, and I'm pretty good at coding too :)</div>
+      <div className="mx-20 my-10">I am a kind, hard-working, team-player with a successful background in military, education, and private industry. Collaborating, mentoring, and learning from others drive my achievements. Honesty, deep connections, and achievement matter to me. I am a passionate learner, always seeking new skills and experiences. I eagerly research and practice to develop new skills. I thrive on complex challenges and take pride in overcoming obstacles. Excellence, integrity, and service guide me. My purpose is to enhance lives through improved products and processes. </div>
     </div>
   )
 }
