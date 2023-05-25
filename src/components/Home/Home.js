d01352adb27558e8abf3b73abdcdf43218df0ebf
Justen Anderson <justenanderson@gmail.com>
2023-05-25 09:27:56 -0600

    Installed Router and set up scroll.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,19 @@
 import { Button } from "@material-tailwind/react";
 
 const Home = () => {
+
+  const handleLearnMore = () => {
+    console.log('This fires')
+    const element = document.querySelector('.about-title')
+    window.scrollTo({
+      top: element?.getBoundingClientRect().top,
+      left: 0,
+      behavior: 'smooth'
+    })
+  }
+
   return (
-    <div className="text-slate-100 text-2xl min-h-screen bg-slate-900 pt-20 flex flex-col items-center">
+    <div className="text-slate-100 text-2xl min-h-9/10 bg-slate-900 pt-20 flex flex-col items-center">
       <h1 className="text-7xl text-center pb-5">Justen Anderson</h1>
       <h2 className="text-4xl text-center text-sky-300">Software Engineer</h2>
       <p className="text-slate-300 pt-5 px-32 text-left">
@@ -14,7 +25,7 @@ const Home = () => {
         processes, and the lives of other people.
       </p>
       <div className="mt-24 ">
-      <Button className='text-sky-300 border-sky-400' variant="outlined">Learn More</Button>
+      <Button onClick= { handleLearnMore }className='mb-36 text-sky-300 border-sky-400' variant="outlined">Learn More</Button>
       </div>
     </div>
   )
