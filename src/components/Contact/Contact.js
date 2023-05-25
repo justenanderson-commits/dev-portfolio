38bcc7b2ea326c4790e8828d138271d2fdcdbd6d
Justen Anderson <justenanderson@gmail.com>
2023-05-24 20:36:42 -0600

    Stylize contact page.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,19 +1,28 @@
+import { Input, Typography, Button, Textarea } from '@material-tailwind/react'
+
 const Contact = () => {
   return (
-    //   <div className="text-slate-100 text-2xl w-full h-screen h-8/10 bg-slate-900 pt-20 flex flex-col items-center">
-    //     <h1 className="text-7xl text-center pb-5">Contact</h1>
-    //     <h2 className="text-4xl text-center text-sky-300">
-    //       Complete the form below
-    //     </h2>
-    //     <p className="text-slate-300 pt-5 px-32 text-left">Contact Form Area</p>
-    //     <div className="mt-24 ">
-    //       {/* <Button className='text-sky-300 border-sky-400' variant="outlined">Submit</Button> */}
-    //     </div>
-    //   </div>
-    // )
     <div className="text-slate-900 text-2xl w-full h-screen h-9/10 bg-white pt-10">
-      <h1 className="text-7xl text-center pb-5">Contact</h1>
-      <div className="mx-36 mt-8">Say Hello </div>
+      <h1 className="text-7xl text-center pb-3">Contact</h1>
+      <div className="mx-auto mt-8 border-slate-900 p-10 border-2 h-2/3 w-2/3 rounded-xl">
+        <form className="w-9/10">
+          <label>Name</label>
+          <Input size="lg" placeholder="John Smith" className='mb-2' />
+          <label>Email</label>
+          <Input size="lg" placeholder="you@example.com"  className='mb-2'/>
+          <label>Message</label>
+          <Textarea
+            size="lg"
+            className="w-full h-32 border-slate-900 border-0 mb-2"
+            placeholder="Share your thoughts"
+          />
+        </form>
+        <div className="m-auto flex justify-center">
+          <Button className="text-sky-600 border-sky-600" variant="outlined">
+            Submit
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
