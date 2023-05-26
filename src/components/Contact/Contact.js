41ae2c8937048edb14993f3cc78673754ff3da1f
Justen Anderson <justenanderson@gmail.com>
2023-05-25 19:42:07 -0600

    Adjust padding on Contact form.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div className="text-slate-900 text-2xl w-full h-screen h-9/10 bg-white pt-10">
       <h1 className="text-7xl text-center pb-3">Contact</h1>
-      <div className="bg-sky-100 mx-auto mt-8 border-slate-900 p-10 border-2 h-2/3 w-2/3 rounded-xl">
+      <div className="bg-sky-100 mx-auto mt-8 border-slate-900 p-10 pb-4 border-2 min-h-2/3 w-2/3 rounded-xl">
         <form className="w-9/10">
           <label>Name</label>
           <Input size="lg" placeholder="John Smith" className='mb-2 bg-white' />
@@ -13,7 +13,7 @@ const Contact = () => {
           <label>Message</label>
           <Textarea
             size="lg"
-            className="w-full h-32 border-slate-900 border-0 mb-2 bg-white"
+            className="w-full h-32 border-slate-900 border-0 mb-4 bg-white"
             placeholder="Share your thoughts"
           />
         </form>
