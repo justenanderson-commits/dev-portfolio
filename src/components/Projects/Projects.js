62702765b33e636b51cf5eafc52f5204b943215a
Justen Anderson <justenanderson@gmail.com>
2023-05-28 14:46:43 -0600

    Fix project padding.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,8 @@ import ProjCard from "../ProjCard/ProjCard";
 
 const Projects = () => {
   return (
-    <div className="text-slate-100 text-2xl w-full  h-screeh-9/10 bg-slate-900 p-10 flex flex-col">
-      <h1 className="text-7xl text-center pb-5">Projects</h1>
+    <div className="text-slate-100 text-2xl w-full  h-screeh-9/10 bg-slate-900 p-8 px-12 flex flex-col">
+      <h1 className="text-7xl text-center pb-2">Projects</h1>
       <div className="grid grid-cols-3 gap-12">
       <ProjCard />
       <ProjCard />
