6de5dae8245c6ccf8eaf7d9d4a74d3152d04326b
Justen Anderson <justenanderson@gmail.com>
2023-05-28 13:57:22 -0600

    Set up page routing.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import Home from '../Home/Home'
 import About from '../About/About'
 import Contact from '../Contact/Contact'
 import Projects from '../Projects/Projects'
+import { Outlet } from 'react-router-dom'
 
 const Main = () => {
   return <div className='main'>
