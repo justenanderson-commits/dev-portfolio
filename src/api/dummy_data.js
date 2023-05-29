7883a4fe1cc4f796d4f9210313e4671ba28bcff0
Justen Anderson <justenanderson@gmail.com>
2023-05-28 23:10:58 -0600

    Add dummy data WIP

---
+++ src/api/dummy_data.js
@@ -0,0 +1,60 @@
+const data = {
+  homeSummary:
+    'Passionate learner and hard worker with a strong record of success in the military, education, and private industry. I value honesty, deep connections, and achievement. I love collaborating, mentoring, and learning from others. Pair-programming is preferred. Complex technical challenges excite me, and I work to purposefully improve products, processes, and the lives of other people.',
+  about:
+    'I am a hard-working, team-player with success in the military, education, and private industry. Collaborating, mentoring, and learning from others drive my achievements. Honesty, deep connections, and achievement matter to me. I am a passionate learner, always seeking new skills and experiences. I eagerly research and practice to develop new skills. I thrive on complex challenges and take pride in overcoming obstacles. Excellence, integrity, and service guide me. My purpose is to enhance lives through improved products and processes. ',
+  education: [
+    {
+      school: 'Turing School of Software and Design',
+      location: 'Denver, CO',
+      startDate: 'August 2022',
+      endDate: 'March 2023',
+      program: 'Front End Engineering Program',
+    },
+    {
+      school: 'Bellevue University',
+      location: 'Bellevue, NE',
+      startDate: 'June 2005',
+      endDate: 'June 2012',
+      program: 'Master of Business Adminstration in Computer Information Systems',
+    },
+    {
+      school: 'Bellevue University',
+      location: 'Bellevue, NE',
+      startDate: 'August 2004',
+      endDate: 'June 2005',
+      program: 'Bachelor of Science in Business Adminstration of Technical Studies',
+    },
+    {
+      school: 'Community College of the Air Force',
+      location: 'Maxwell AFB, LA',
+      startDate: 'October 2001',
+      endDate: 'March 2005',
+      program: 'Bachelor of Science in Business Adminstration of Technical Studies',
+    }
+  ],
+  experience: [],
+  certsAwards: [],
+  projects: [{
+    name: 'Menuify',
+    description: 'Menu application for restaurant owners to create beautiful, intuitive menus.',
+    url:'https://menu-ify.vercel.app/',
+    img:'',
+    gif:''
+  },
+  {
+    name: 'Pawfect Match',
+    description: 'Dog finder app for people to research breeds based on their lifestyle and preferences.',
+    url:'https://pawfect-match.vercel.app/',
+    img:'',
+    gif:''
+  },{
+    name: 'Rancid Tomatillos',
+    description: 'Movie collection application that allows users to filter titles by rating.',
+    url:'https://rancid-tomatillos-puce.vercel.app/',
+    img:'',
+    gif:''
+  },]
+}
+
+export default data
