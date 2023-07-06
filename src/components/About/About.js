7389edc84a269cf89a661745b07ea9f7a37228d4
Justen Anderson <justenanderson@gmail.com>
2023-07-06 14:39:49 -0500

    Adjust resume button.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ import { Button } from '@material-tailwind/react'
 
 const About = ({ about, experience }) => {
   return (
-    <div className="relative about text-slate-900 text-2xl w-full min-h-screen h-auto h-9/10 bg-white">
+    <div className="relative about text-slate-900 text-2xl w-full min-h-screen h-auto h-9/10 bg-white mb-4">
       <h1 className="about-title text-7xl text-center pb-2 mt-8">About</h1>
       <div className="mx-36 mt-8">
         { about } </div>
@@ -13,8 +13,8 @@ const About = ({ about, experience }) => {
       <Experience experience={ experience } />
       <CertsAwards />
        <div className="mx-auto flex justify-center">
-          <Button className="text-sky-600 border-sky-600 bg-white" variant="outlined">
-            Full Resume
+          <Button className="text-sky-600 mt-4 border-sky-600 bg-white" variant="outlined">
+            Resume
           </Button>
         </div>
     </div>
