1c37fe0ee132fe74ee112c3577395d4c6474d758
Justen Anderson <justenanderson@gmail.com>
2023-07-08 14:09:52 -0500

    Added react icons to the contact page.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,17 @@
 import { Input, Typography, Button, Textarea } from '@material-tailwind/react'
+import { BsGithub } from 'react-icons/bs'
+import { ImMail } from 'react-icons/im'
+import { BsLinkedin } from 'react-icons/bs'
+
 
 const Contact = () => {
   return (
     <div className="text-slate-900 text-2xl w-full min-h-screen bg-white pt-10">
       <h1 className="text-7xl text-center pb-3">Contact</h1>
-      <div className="bg-sky-100 mx-auto mt-8 border-slate-900 p-10 pb-4 border-2 min-h-2/3 w-2/3 rounded-xl">
+      <div className="bg-sky-100 mx-auto mt-8 border-slate-900 p-10 border-2 min-h-2/3 w-2/3 rounded-xl flex justify-evenly ">
+        <h1><BsGithub /> </h1>
+        <h1><ImMail /> </h1>
+        <h1><BsLinkedin /> </h1>
         {/* <form className="w-9/10">
           <label>Name</label>
           <Input size="lg" placeholder="John Smith" className='mb-2 bg-white' />
