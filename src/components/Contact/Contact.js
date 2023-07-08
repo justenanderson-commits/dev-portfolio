747ae205aaabfa74ae9bc3e5816a362d80364e16
Justen Anderson <justenanderson@gmail.com>
2023-07-08 14:23:23 -0500

    Add instructions to the contact page.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
   return (
     <div className="text-slate-900 text-2xl w-full min-h-screen bg-white pt-10">
       <h1 className="text-7xl text-center pb-3">Contact</h1>
-      
+      <h2 className="text-2xl flex justify-center mt-4">I'd love to hear from you. Use any of the links below to get in touch:</h2>
       <div className="bg-sky-100 mx-auto mt-8 border-slate-900 p-10 border-2 min-h-2/3 w-2/3 rounded-xl flex justify-evenly text-8xl">
         <a href="https://github.com/justenanderson-commits"
         target="blank"> 
