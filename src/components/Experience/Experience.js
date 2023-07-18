e87b9b414ccea5d3eb70eb631eba21dbd94394cb
Justen Anderson <justenanderson@gmail.com>
2023-07-18 14:41:52 -0600

    Complete outline of professional experience.

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,28 +1,31 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState } from 'react'
 import ExpCard from '../ExpCard/ExpCard'
 import {
   Accordion,
   AccordionHeader,
   AccordionBody,
-} from "@material-tailwind/react";
+} from '@material-tailwind/react'
 
 const Experience = ({ experience }) => {
   console.log('Experience: ', experience)
-  const [open, setOpen] = useState(0);
- 
+  const [open, setOpen] = useState(0)
+
   const handleOpen = (value) => {
-    setOpen(open === value ? 0 : value);
-  };
- 
+    setOpen(open === value ? 0 : value)
+  }
+
   const customAnimation = {
     mount: { scale: 1 },
     unmount: { scale: 0.9 },
   }
   return (
     <div className="border-2 border-slate-900 mx-36 my-4 rounded-lg">
-       <Fragment>
+      <Fragment>
         <Accordion open={open === 1}>
-          <AccordionHeader className='p-2 bg-sky-100' onClick={() => handleOpen(1)}>
+          <AccordionHeader
+            className="p-2 bg-sky-100"
+            onClick={() => handleOpen(1)}
+          >
             Experience
           </AccordionHeader>
           <AccordionBody className="p-4">
@@ -38,42 +41,37 @@ const Experience = ({ experience }) => {
               more.
             </p>
             <br />
-            <h1 className="text-xl font-bold">Multiple Schools in Colorado & Costa Rica</h1>
+            <h1 className="text-xl font-bold">
+              Multiple Schools in Colorado & Costa Rica
+            </h1>
             <h2 className="text-lg font-semibold">
-            Professional Educator & Department Leader - August 2008 to July 2022 
+              Professional Educator - August 2008 to July 2022
             </h2>
-            <p className="text-lg">
-              Graduate program covering finance, accounting, marketing, media,
-              logistics, performance, innovation, and more with a concentration
-              on technology as a large-scale business solution.
-            </p>
+            <p className="text-lg">Teaching synopsis</p>
             <h2 className="text-lg font-semibold">
-              Bachelor of Science - June 2005
+              Department Lead - August 2016 to July 2022
             </h2>
-            <p className="text-lg">
-              Business Adminstration in Technical Studies - A program designed
-              to to enhance both the knowledge and technological skills of
-              business leaders.
-            </p>
+            <p className="text-lg">Leadership synopsis</p>
             <br />
             <h1 className="text-xl font-bold">
-              Community College of the Air Force
+              California Casualty Insurance Company
             </h1>
             <label className="text-lg font-semibold">
-              Associate of Applied Science - March 2005
+              Auto and Home Insurance Agent - April 2006 to July 2008
             </label>
-            <p className="text-lg">
-              A vocational degree based on technical training covering
-              electronic and computer science fundamentals, as well as avionic
-              systems for communication, navigation, weapons and radar systems.
-            </p>
+            <p className="text-lg">Insurance synopsis</p>
+            <br />
+            <h1 className="text-xl font-bold">United States Air Force</h1>
+            <label className="text-lg font-semibold">
+              Avionics and Automated Test Station Technician - October 2001 to
+              April 2006
+            </label>
+            <p className="text-lg">USAF synopsis</p>
           </AccordionBody>
         </Accordion>
       </Fragment>
-      </div>
-    );
+    </div>
+  )
 }
 
 export default Experience
-
-
