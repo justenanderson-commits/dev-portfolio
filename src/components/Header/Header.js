3f287d43aae4985ea623acbc737203ac345e2215
Justen Anderson <justenanderson@gmail.com>
2023-08-10 14:15:40 -0600

    Add the mobile drop down menu.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
-import { Navbar, Typography, MobileNav } from '@material-tailwind/react'
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  IconButton
+} from '@material-tailwind/react'
 
 const Header = () => {
   const [openNav, setOpenNav] = React.useState(false)
+
   React.useEffect(() => {
     window.addEventListener(
       'resize',
@@ -32,7 +38,6 @@ const Header = () => {
           About
         </a>
       </Typography>
-
       <Typography
         as="li"
         variant="small"
@@ -55,30 +60,61 @@ const Header = () => {
       </Typography>
     </ul>
   )
-
   return (
-    <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-0 px-0 lg:px-8 lg:py-4 bg-slate-900">
-      <div className="flex items-center justify-between text-blue-gray-900">
-        <div className="flex">
-          <a
-            href="https://github.com/justenanderson-commits/dev-portfolio/tree/main"
-            target="blank"
-          >
-            <Typography
-            as="a"
-            href="https://github.com/justenanderson-commits/dev-portfolio/tree/main"
-            target="blank"
-            className="ml-2 font-bold text-sky-500 cursor-pointer text-md"
+      <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-0 px-0 lg:px-8 lg:py-4 bg-slate-900">
+        <div className="flex items-center justify-between text-blue-gray-900">
+              <Typography
+                as="a"
+                href="https://github.com/justenanderson-commits/dev-portfolio/tree/main" target="blank"
+                className="ml-2 font-bold text-sky-500 cursor-pointer text-md"
+              >
+                REPO
+              </Typography>
+          <div className="flex items-center gap-4">
+            <div className="mr-4 hidden lg:block">{navList}</div>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
             >
-            Repo
-            </Typography>
-          </a>
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
         </div>
-        <div className="flex items-center gap-4">
-          <div className="mr-4 hidden lg:block">{navList}</div>
-        </div>
-      </div>
-    </Navbar>
+        <MobileNav open={openNav}>
+          {navList}
+        </MobileNav>
+      </Navbar>
   )
 }
 
