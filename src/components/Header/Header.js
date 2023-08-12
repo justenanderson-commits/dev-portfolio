fb735be30336d432d8941b20ddce10f0aef348e2
Justen Anderson <justenanderson@gmail.com>
2023-08-12 12:59:49 -0600

    Replaced MobileNav with Collapse.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import {
   Navbar,
-  MobileNav,
   Typography,
-  IconButton
+  IconButton, 
+  Collapse,
+  Card,
+  Button,
+  CardBody
 } from '@material-tailwind/react'
 
 const Header = () => {
   const [openNav, setOpenNav] = React.useState(false)
+  const [open, setOpen] = React.useState(false);
+ 
+  const toggleOpen = () => setOpen((cur) => !cur);
 
   React.useEffect(() => {
     window.addEventListener(
@@ -15,6 +21,8 @@ const Header = () => {
       () => window.innerWidth >= 960 && setOpenNav(false)
     )
   }, [])
+
+
 
   const navList = (
     <ul className="mb-0 mt-0 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
@@ -72,10 +80,10 @@ const Header = () => {
               </Typography>
           <div className="flex items-center gap-4">
             <div className="mr-4 hidden lg:block">{navList}</div>
-            <IconButton
+            {/* <IconButton
               variant="text"
               className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-              ripple={false}
+              ripple={true}
               onClick={() => setOpenNav(!openNav)}
             >
               {openNav ? (
@@ -108,12 +116,25 @@ const Header = () => {
                   />
                 </svg>
               )}
-            </IconButton>
+            </IconButton> */}
           </div>
         </div>
-        <MobileNav open={openNav}>
+        {/* <MobileNav open={openNav}>
           {navList}
-        </MobileNav>
+        </MobileNav> */}
+
+        <>
+      <Button onClick={toggleOpen}>Open Collapse</Button>
+      <Collapse open={open}>
+        <Card className="my-4 mx-auto w-8/12">
+          <CardBody>
+            <Typography>
+              {navList}
+            </Typography>
+          </CardBody>
+        </Card>
+      </Collapse>
+    </>
       </Navbar>
   )
 }
