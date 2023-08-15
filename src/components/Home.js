f13da00d23c5d810c99a1349fa3eee1d6bfe0f6d
Justen Anderson <justenanderson@gmail.com>
2023-08-15 12:31:55 -0600

    Remove extra spacing

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ const Home = ({ homeSummary }) => {
       className="relative home-page text-slate-100 text-2xl min-h-screen bg-slate-900 pt-8 lg:pt-20 flex flex-col items-center"
     >
       <h1 className="text-5xl lg:text-7xl text-center pb-5">Justen Anderson</h1>
-      <h2 className="text-3xl lg:text-4xl text-center text-sky-300">Developer | Educator  |  Veteran</h2>
+      <h2 className="text-3xl lg:text-4xl text-center text-sky-300">Developer | Educator | Veteran</h2>
       <p className="text-slate-300 pt-5 px-8 text-left lg:px-32">
         {homeSummary}
       </p>
