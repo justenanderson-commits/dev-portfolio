e7f6353104607f9ca3b0a32f18af1fbcadf00e6c
Justen Anderson <justenanderson@gmail.com>
2023-10-27 12:18:20 -0600

    Add first passing tests

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,7 +1,9 @@
-const { test, expect } = require('@playwright/test')
+import { test, expect } from '@playwright/test';
 
-test('has title', async({ page }) => {
-  await page.goto('https://justenanderson.dev')
-  await expect(page).toHaveTitle(/Welcome!/)
-})
-
+test('test', async ({ page }) => {
+  await page.goto('https://www.justenanderson.dev/');
+  // await expect(page).toContainText('Justen Anderson')
+  await expect(page.getByRole('heading', { name: 'Developer | Educator | Veteran' })).toContainText('Developer | Educator | Veteran')
+  // await page.getByRole('button', { name: 'Learn More' }).click();
+  // await page.getByRole('heading', { name: 'About' }).click();
+});
