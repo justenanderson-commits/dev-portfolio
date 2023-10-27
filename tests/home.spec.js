21989458a0f45807c4ba48ab1bb137c8f2dce6f4
Justen Anderson <justenanderson@gmail.com>
2023-10-27 14:05:13 -0600

    Add test for paragraph text

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -8,4 +8,7 @@ test('test', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Justen Anderson'})).toContainText('Justen Anderson')
   
   await expect(page.getByRole('heading', { name: 'Developer | Educator | Veteran' })).toContainText('Developer | Educator | Veteran')
+
+  await expect(page.getByText('A passionate learner and hard worker with excellent results in the military, edu')).toHaveClass('text-slate-300 pt-5 px-8 text-left lg:px-32')
+
 });
