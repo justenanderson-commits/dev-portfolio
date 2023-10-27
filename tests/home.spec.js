f459e5f1799db8d05432a40bfb2a781dcaf050f9
Justen Anderson <justenanderson@gmail.com>
2023-10-27 12:30:38 -0600

    Organize tests for readability

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -2,8 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://www.justenanderson.dev/');
-  // await expect(page).toContainText('Justen Anderson')
+
+  await expect(page).toHaveTitle('Welcome!')
+  
+  await expect(page.getByRole('heading', { name: 'Justen Anderson'})).toContainText('Justen Anderson')
+  
   await expect(page.getByRole('heading', { name: 'Developer | Educator | Veteran' })).toContainText('Developer | Educator | Veteran')
-  // await page.getByRole('button', { name: 'Learn More' }).click();
-  // await page.getByRole('heading', { name: 'About' }).click();
 });
