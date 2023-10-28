097e9604dc4f7e993b8fcac62e9ded5dae785409
Justen Anderson <justenanderson@gmail.com>
2023-10-28 15:12:37 -0600

    Add tests to ensure the Projects and Contact page are visible on page load.

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -12,4 +12,8 @@ test('home page content', async ({ page }) => {
   await expect(page.getByText('A passionate learner and hard worker with excellent results in the military, edu')).toHaveClass('text-slate-300 pt-5 px-8 text-left lg:px-32')
 
   await expect(page.getByRole('heading', { name: 'About'})).toBeVisible()
+
+  await expect(page.getByRole('heading', { name: 'Projects'})).toBeVisible()
+
+  await expect(page.getByRole('heading', { name: 'Contact'})).toBeVisible()
 });
