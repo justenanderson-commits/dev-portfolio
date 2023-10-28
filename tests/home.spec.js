71ba62ac4861b0d4e0a7fca6fb774ba12d2be877
Justen Anderson <justenanderson@gmail.com>
2023-10-28 15:31:26 -0600

    Refactor test for readability

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -5,9 +5,9 @@ test('home page', async ({ page }) => {
 
   await expect(page).toHaveTitle('Welcome!')
   
-  await expect(page.getByRole('heading', { name: 'Justen Anderson'})).toContainText('Justen Anderson')
+  await expect(page.getByRole('heading', { name: 'Justen Anderson'})).toBeVisible()
   
-  await expect(page.getByRole('heading', { name: 'Developer | Educator | Veteran' })).toContainText('Developer | Educator | Veteran')
+  await expect(page.getByRole('heading', { name: 'Developer | Educator | Veteran' })).toBeVisible()
 
   await expect(page.getByText('A passionate learner and hard worker with excellent results in the military, edu')).toHaveClass('text-slate-300 pt-5 px-8 text-left lg:px-32')
 
