0d7464824a2d8ded214c998c2ed294c7a3e07b40
Justen Anderson <justenanderson@gmail.com>
2023-10-28 16:14:29 -0600

    Add test for Education accordian menu

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,32 +1,50 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('home page', async ({ page }) => {
-  await page.goto('https://www.justenanderson.dev/');
+  await page.goto('https://www.justenanderson.dev/')
 
   await expect(page).toHaveTitle('Welcome!')
-  
-  await expect(page.getByRole('heading', { name: 'Justen Anderson'})).toBeVisible()
-  
-  await expect(page.getByRole('heading', { name: 'Developer | Educator | Veteran' })).toBeVisible()
 
-  await expect(page.getByText('A passionate learner and hard worker with excellent results in the military, edu')).toHaveClass('text-slate-300 pt-5 px-8 text-left lg:px-32')
+  await expect(
+    page.getByRole('heading', { name: 'Justen Anderson' })
+  ).toBeVisible()
 
-  await expect(page.getByRole('heading', { name: 'About'})).toBeVisible()
+  await expect(
+    page.getByRole('heading', { name: 'Developer | Educator | Veteran' })
+  ).toBeVisible()
 
-  await expect(page.getByRole('heading', { name: 'Projects'})).toBeVisible()
+  await expect(
+    page.getByText(
+      'A passionate learner and hard worker with excellent results in the military, edu'
+    )
+  ).toHaveClass('text-slate-300 pt-5 px-8 text-left lg:px-32')
 
-  await expect(page.getByRole('heading', { name: 'Contact'})).toBeVisible()
+  await expect(page.getByRole('heading', { name: 'About' })).toBeVisible()
+
+  await expect(page.getByRole('heading', { name: 'Projects' })).toBeVisible()
+
+  await expect(page.getByRole('heading', { name: 'Contact' })).toBeVisible()
 
   await page.getByText('Learn More').click()
 
-  await expect(page.getByRole('heading', { name: 'About'})).toBeInViewport()
-});
+  await expect(page.getByRole('heading', { name: 'About' })).toBeInViewport()
+})
 
-test('about page', async({ page }) => {
+test('about page', async ({ page }) => {
   await page.goto('https://www.justenanderson.dev/about')
 
-  await expect(page.getByRole('heading', { name: 'About'})).toBeVisible()
+  await expect(page.getByRole('heading', { name: 'About' })).toBeVisible()
 
-  await expect(page.getByText('A hard-working, team-player with success in the military, education, and private industry.')).toBeVisible() 
+  await expect(
+    page.getByText(
+      'A hard-working, team-player with success in the military, education, and private industry.'
+    )
+  ).toBeVisible()
 
+  await page.getByRole('button', { name: 'Education' }).click();
+  
+  const title = await page.getByRole('heading', { name: 'Turing School of Software and Design - Denver, Colorado' }).first()
+  
+  await expect(title).toBeInViewport()
+  await page.screenshot({ path: 'screenshot1.png' })
 })
