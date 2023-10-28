bed6018b05b6960984d00b30cada6755aa25fb21
Justen Anderson <justenanderson@gmail.com>
2023-10-28 15:43:38 -0600

    Add test for About page paragraph

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -27,5 +27,6 @@ test('about page', async({ page }) => {
 
   await expect(page.getByRole('heading', { name: 'About'})).toBeVisible()
 
-  
+  await expect(page.getByText('A hard-working, team-player with success in the military, education, and private industry.')).toBeVisible() 
+
 })
